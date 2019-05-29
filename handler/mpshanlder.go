5cb09664e38ac9a26fe35eed581e01b5ddb5e6db
kyang <yk1106>
2019-05-29 16:47:33 +0800

    gi

--- handler/mpshanlder.go
+++ handler/mpshanlder.go
@@ -7,6 +7,7 @@ import (
 	"ampp-server/common/utils"
 
 	"github.com/go-xorm/xorm"
+	"github.com/yakaa/log4g"
 )
 
 type (
@@ -15,7 +16,7 @@ type (
 	}
 )
 
-func NewMpsHandler(mpsMysql *xorm.Engine) (*MpsHandler) {
+func NewMpsHandler(mpsMysql *xorm.Engine) *MpsHandler {
 
 	return &MpsHandler{mpsMysql: mpsMysql}
 }
@@ -25,19 +26,22 @@ func (h *MpsHandler) Consumer(message *rabbitmq.Message) error {
 	_, err := h.mpsMysql.Transaction(func(session *xorm.Session) (i interface{}, e error) {
 		var err error
 		var res sql.Result
+		query := ""
+		ks := ""
+		placeholder := ""
+		vs := []interface{}(nil)
 		switch message.Operate {
 		case rabbitmq.InsertType:
-			ks, placeholder, vs := utils.SqlBuild(message.Data, ",")
-			query := "insert into " + message.DataBase + "." + message.Table + " (" + ks + ") values (" + placeholder + ")"
-			res, err = session.Exec(query, vs)
+			ks, placeholder, vs = utils.SqlBuild(message.Data, ",")
+			query = "insert into " + message.DataBase + "." + message.Table + " (" + ks + ") values (" + placeholder + ")"
 		case rabbitmq.DeleteType:
-			query := "delete from " + message.DataBase + "." + message.Table + " where " + message.Condition
-			res, err = session.Exec(query)
+			query = "delete from " + message.DataBase + "." + message.Table + " where " + message.Condition
 		case rabbitmq.UpdateType:
-			ks, _, vs := utils.SqlBuild(message.Data, "=?,")
-			query := "update  " + message.DataBase + "." + message.Table + " sett " + ks + " where " + message.Condition
-			res, err = session.Exec(query, vs)
+			ks, _, vs = utils.SqlBuild(message.Data, "=?,")
+			query = "update  " + message.DataBase + "." + message.Table + " sett " + ks + " where " + message.Condition
 		}
+		log4g.InfoFormat("Exec sql ===> %s ,ks is %+v, build data %+v", query, ks, vs)
+		res, err = session.Exec(query, vs)
 		return res, err
 	})
 	return err
